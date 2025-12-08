3cf385b3c330470185dadddf231e791755abb0ee
SAWYER SAWYER <mehaleka240399@yandex.ru>
2025-12-08 23:44:00 +0300

    fix: исправлен цвет - добавлены переменные

--- src/shared/ui/Loader/Loader.tsx
+++ src/shared/ui/Loader/Loader.tsx
@@ -15,13 +15,13 @@ export const Loader: React.FC<LoaderProps> = ({ size = 80 }) => {
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <rect width="40" height="40" rx="20" fill="#ABD27A" />
+        <rect width="40" height="40" rx="20" fill="var(--color-accent)" />
 
         {/* вращающаяся и пульсирующая звезда */}
         <g className={styles.innerStar}>
           <path
             d="M20 10C20 15.5228 24.4772 20 30 20C24.4772 20 20 24.4772 20 30C20 24.4772 15.5228 20 10 20C15.5228 20 20 15.5228 20 10Z"
-            fill="#F9FAF7"
+            fill="var(--color-background)"
           />
         </g>
       </svg>
