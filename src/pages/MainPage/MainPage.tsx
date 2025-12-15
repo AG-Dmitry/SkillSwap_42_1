bea139bbd2abef371343d1fb668f7bdbd9a9f9ec
creator-of-masterpieces <timonov.dim13@gmail.com>
2025-12-15 10:32:39 +0300

    feat(MainPage): Зафиксировал блок фильтров при скролле;
    Зафиксировал блок фильтров при скролле;
    Уменьшил ширину фильтров (появлялся горизонтальный скролл);
    Установил обертке блока фильтров высоту в 100vh и добавил вертикальный скролл;

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -44,11 +44,13 @@ export const MainPage = () => {
   return (
     <div className={styles.main}>
       <aside className={styles.filterContainer}>
-        <Filter
-          filters={filters}
-          onFiltersChange={setFilters}
-          onClearSearchQuery={clearSearchQuery}
-        />
+        <div className={styles.filterWrap}>
+          <Filter
+            filters={filters}
+            onFiltersChange={setFilters}
+            onClearSearchQuery={clearSearchQuery}
+          />
+        </div>
       </aside>
       <section className={styles.galleryContainer}>
         {isLoading && (
