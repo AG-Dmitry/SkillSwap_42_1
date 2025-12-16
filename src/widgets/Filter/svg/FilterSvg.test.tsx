42de7b1a00cdb74ea931abccf44b34c494756e58
Moro <hound90@mail.ru>
2025-12-16 16:53:34 +0500

    feat: Добавил unit-test на hooks и reducers с покрытием выше 70%.

--- src/widgets/Filter/svg/FilterSvg.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import { ClearSVG } from "./FilterSvg";
-
-describe("ClearSVG", () => {
-  it("должен рендериться", () => {
-    const { container } = render(<ClearSVG />);
-    expect(container.firstChild).toBeInTheDocument();
-  });
-});
--- src/widgets/UserCardsSection/svg/SortSvg.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import { SortSvg } from "./SortSvg";
-
-describe("SortSvg", () => {
-  it("должен рендериться", () => {
-    const { container } = render(<SortSvg />);
-    expect(container.firstChild).toBeInTheDocument();
-  });
-});
